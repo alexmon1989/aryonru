72374634af816b704440f478b5440c06f8c61021
Monastyretsky Alexandr <alex.mon1989@gmail.com>
2014-11-10 23:31:50 +0200

    - удаление строки <strong>This</strong> is an example of a <em>HTML</em> caption with <a href="#">a link</a>, которая вылазит поисковиках

--- fuel/app/modules/main/views/template/slider.php
+++ fuel/app/modules/main/views/template/slider.php
@@ -4,9 +4,6 @@
             <?php echo Html::anchor(Uri::create($slide->uri), Asset::img('slider/' . $slide->img_path)); ?>
         <?php endforeach; ?>
     </div>
-    <div id="htmlcaption" class="nivo-html-caption">
-        <strong>This</strong> is an example of a <em>HTML</em> caption with <a href="#">a link</a>.
-    </div>
 </div>
 
 <script type="text/javascript">
